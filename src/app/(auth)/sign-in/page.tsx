1ba4a7f0a93a3c713d4c76cdfda71ad664ba0937
nischal1101 <kaflenk123@gmail.com>
2024-10-17 15:32:35 +0545

    add sign-in api route from next-auth

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -8,7 +8,6 @@ const Login = () => {
     <div className="flex h-screen items-center justify-center  px-4 sm:px-6 lg:px-8">
       <div className="w-96 p-8 shadow-2xl ">
         <h1 className="py-8 text-center text-4xl text-primary">MedRepo.</h1>
-
         <form>
           <div className="relative flex flex-col items-center justify-center">
             <Mail className="absolute left-2 top-[0.65rem] " size={15} />
@@ -30,21 +29,7 @@ const Login = () => {
               className="input input-bordered w-full max-w-xs px-8 transition delay-150 ease-in-out hover:border-gray-500 "
             />
           </div>
-          {/* <div className="  my-4">
-            <div className="flex items-center justify-between">
-              <Select>
-                <SelectTrigger className="w-[100px]">
-                  <SelectValue placeholder="Role" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="patient">Patient</SelectItem>
-                  <SelectItem value="doctor">Doctor</SelectItem>
-                  <SelectItem value="hospital">Hospital</SelectItem>
-                </SelectContent>
-              </Select>
-              <p className="">Forgot Password</p>
-            </div>
-          </div> */}
+
           <Button
             type="submit"
             className="btn btn-primary mt-4 w-full max-w-xs "
