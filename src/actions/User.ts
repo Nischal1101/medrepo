ec632f9fa95e871a6a5eef1cce809e3f40441978
nischal1101 <kaflenk123@gmail.com>
2024-10-30 20:41:09 +0545

    use server action with zod

--- src/actions/User.ts
+++ src/actions/User.ts
@@ -1,14 +1,37 @@
 "use server";
 import { signIn } from "@/lib/auth";
+import { AuthError } from "next-auth";
 
-export const credentialsSignIn = async (email: string, password: string) => {
-  if (!email || !password) {
-    return;
+export const credentialsSignIn = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    if (!email || !password) {
+      return { error: "Invalid Credentials" };
+    }
+    await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+      provider: "credentials",
+    });
+  } catch (error: unknown) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            error: "Invalid credentials",
+          };
+        default:
+          return {
+            error: "Something went wrong.",
+          };
+      }
+    }
+    throw error;
   }
-  await signIn("credentials", {
-    redirect: false,
-    email,
-    password,
-    provider: "credentials",
-  });
 };
