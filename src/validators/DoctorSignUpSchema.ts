973f8d8d7e26f196b7baf1153c40a586e9d34d0a
nischal1101 <kaflenk123@gmail.com>
2024-11-17 11:55:25 +0545

    build doctor signup schema

---
+++ src/validators/DoctorSignUpSchema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+export const loginSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(5, { message: "Password must be atleaset 5 characters long" })
+    .max(20, { message: "Password can't be more than 20 characters long" }),
+});
