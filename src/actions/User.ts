aa0b38fe711abe86bb8ee18ef4b170528cfd6221
nischal1101 <kaflenk123@gmail.com>
2024-11-20 20:43:55 +0545

    add patient signup

--- src/actions/User.ts
+++ src/actions/User.ts
@@ -1,5 +1,8 @@
 "use server";
 import { signIn } from "@/lib/auth";
+import db from "@/lib/db/db";
+import { PatientTable, UserTable } from "@/lib/db/Schema";
+import { eq } from "drizzle-orm";
 import { AuthError } from "next-auth";
 export const credentialsSignIn = async ({
   email,
@@ -9,28 +12,59 @@ export const credentialsSignIn = async ({
   password: string;
 }) => {
   try {
-    if (!email || !password) {
-      return { error: "Invalid Credentials" };
-    }
     await signIn("credentials", {
       redirect: false,
       email,
       password,
-      provider: "credentials",
     });
   } catch (error: unknown) {
     if (error instanceof AuthError) {
       switch (error.type) {
         case "CredentialsSignin":
           return {
-            error: "Invalid credentials",
+            error: error.cause,
           };
         default:
           return {
-            error: "Something went wrong.",
+            error: error.message,
           };
       }
     }
     throw error;
+  }
+};
+export const credentialsPatientSignUp = async ({
+  email,
+  password,
+  phone,
+  dob,
+  name,
+}: {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  dob: Date;
+}) => {
+  try {
+    if (!email || !password || !phone || !dob) {
+      return { error: "Invalid Credentials" };
+    }
+    const exists = await db
+      .select()
+      .from(UserTable)
+      .where(eq(UserTable.email, email));
+    if (exists.length > 0) {
+      return { error: "Email already exists, please login." };
+    }
+    const user = await db
+      .insert(UserTable)
+      .values({ name, email, password })
+      .returning();
+    await db.insert(PatientTable).values({ dob: dob.toISOString(), phone, userId: user[0].id });
+  } catch (error: unknown) {
+    return {
+      error: "Something went wrong. " + (error instanceof Error ? error.message : String(error)),
+    };
   }
 };
