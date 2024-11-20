620ce0a19ec01bbf8cb7b3f1f9d5b56faa3a2e2a
nischal1101 <kaflenk123@gmail.com>
2024-11-20 07:43:10 +0545

    remove google auth

---
+++ src/app/(auth)/sign-in/_components/SigninForm.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { credentialsSignIn } from "@/actions/User";
+import AuthBtn from "@/components/AuthBtn";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { loginSchema } from "@/validators/LoginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LockKeyhole, Mail } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+export default function SignInForm() {
+  const router = useRouter();
+  type Schema = z.infer<typeof loginSchema>;
+  const form = useForm<Schema>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(loginSchema),
+  });
+  const onSubmit = async (data: Schema) => {
+    try {
+      const error = await credentialsSignIn(data);
+      if (error) {
+        toast.error(String(error));
+      } else {
+        toast.success("User logged in Successfully!");
+      }
+      router.refresh();
+    } catch (err: unknown) {
+      toast.error("Something went wrong" + err);
+    }
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="mt-2 grid">
+              <FormControl>
+                <div className="relative flex flex-col items-center justify-center">
+                  <Mail className="absolute left-2 top-[0.65rem] " size={15} />
+                  <Input
+                    placeholder="Enter your email"
+                    id="email"
+                    {...field}
+                    className={`w-full max-w-xs px-8 transition delay-150 ease-in-out hover:border-gray-500 `}
+                  />
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="mt-2 grid">
+              <FormControl>
+                <div className="relative flex flex-col items-center justify-center ">
+                  <LockKeyhole
+                    className="absolute left-2 top-[0.65rem]"
+                    size={15}
+                  />
+                  <Input
+                    type="password"
+                    {...field}
+                    placeholder=" Your Password"
+                    id="password"
+                    className={` w-full max-w-xs px-8 transition delay-150 ease-in-out hover:border-gray-500 `}
+                  />
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <AuthBtn title={"Log In"} className="mt-6" />
+      </form>
+    </Form>
+  );
+}
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components/ui/button";
 
-import { auth, signIn } from "@/lib/auth";
-import Image from "next/image";
+import { auth } from "@/lib/auth";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import SignInForm from "./_components/SigninForm";
@@ -16,32 +14,7 @@ const Login = async () => {
       <div className="w-96 p-8 shadow-2xl ">
         <h1 className="py-8 text-center text-4xl text-primary">MedRepo.</h1>
         <SignInForm />
-        <div className="mt-2 flex items-center gap-2">
-          <div className="h-px w-1/2 bg-gray-200"></div>
-          <span>or</span>
-          <div className="h-px w-1/2 bg-gray-200"></div>
-        </div>
-        <form
-          action={async () => {
-            "use server";
-            await signIn("google");
-          }}
-        >
-          <Button
-            variant={"outline"}
-            className="mt-4 w-full rounded-lg border border-black"
-          >
-            <div className="flex items-center justify-center gap-4">
-              <Image
-                src="/assets/google.png"
-                height={20}
-                width={30}
-                alt="loading google image"
-              />
-              <span>Sign In with Google</span>
-            </div>
-          </Button>
-        </form>
+
         <hr className="my-8" />
         <div className="mt-3 text-center">
           Need an account? Register as
