3c425600c49428d64009cf7a54630fa3d21dfce2
nischal1101 <kaflenk123@gmail.com>
2024-11-20 08:36:05 +0545

    add name field

--- src/validators/PatientSignUpSchema.ts
+++ src/validators/PatientSignUpSchema.ts
@@ -5,8 +5,12 @@ export const patientSignUpSchema = z.object({
     .email({ message: "Invalid email address" }),
   password: z
     .string({ required_error: "password is a required field" })
-    .min(5, { message: "Password must be atleaset 5 characters long" })
+    .min(5, { message: "Password must be at least 5 characters long" })
     .max(20, { message: "Password can't be more than 20 characters long" }),
+  name: z
+    .string({ required_error: "name is a required field" })
+    .min(5, { message: "name must be at least 3 characters long" })
+    .max(20, { message: "name can't be more than 20 characters long" }),
   phone: z
     .string({ required_error: "phone is a required field" })
     .min(10)
