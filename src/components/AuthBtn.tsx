74cfb3f5e3cb84b90d7c80ef0b21654f1a3ad10b
nischal1101 <kaflenk123@gmail.com>
2024-11-20 08:03:08 +0545

    add className props

--- src/components/AuthBtn.tsx
+++ src/components/AuthBtn.tsx
@@ -1,11 +1,21 @@
 import { LoaderCircle } from "lucide-react";
 import { useFormStatus } from "react-dom";
 import { Button } from "./ui/button";
-const AuthBtn = ({ title }: { title: string }) => {
+const AuthBtn = ({
+  title,
+  className,
+}: {
+  title: string;
+  className?: string;
+}) => {
   const { pending } = useFormStatus();
   return (
     <>
-      <Button type="submit" disabled={pending} className="mt-4 w-full max-w-xs">
+      <Button
+        type="submit"
+        disabled={pending}
+        className={`mt-4 w-full max-w-xs ${className}`}
+      >
         {pending && <LoaderCircle className="mr-3  animate-spin" />}
         {title}
       </Button>
