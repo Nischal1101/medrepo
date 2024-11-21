cb2a8e12ff2f34b1bdd0a816f85dbc9e6aa6bf80
nischal1101 <kaflenk123@gmail.com>
2024-11-21 16:40:55 +0545

    add enum for specialization

--- src/validators/DoctorSignUpSchema.ts
+++ src/validators/DoctorSignUpSchema.ts
@@ -1,8 +1,15 @@
+import { DoctorSpecialization } from "@/constants";
 import z from "zod";
-export const loginSchema = z.object({
+
+export const doctorSignUpSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
     .min(5, { message: "Password must be atleaset 5 characters long" })
     .max(20, { message: "Password can't be more than 20 characters long" }),
+  name: z
+    .string({ required_error: "name is a required field" })
+    .min(3, { message: "name must be at least 3 characters long" })
+    .max(20, { message: "name can't be more than 20 characters long" }),
+  specialization: z.enum(DoctorSpecialization),
 });
