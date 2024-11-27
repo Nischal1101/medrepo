113f89fb5583239001dd21a9ca41a077e37a4162
nischal1101 <kaflenk123@gmail.com>
2024-11-27 19:41:37 +0545

    add loading for patient page with patient table skeleton

---
+++ drizzle/0006_many_malice.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "doctors" ADD COLUMN "doctor_name" varchar(255);--> statement-breakpoint
+ALTER TABLE "patients" ADD COLUMN "patient_name" varchar(255);
---
+++ src/app/(home)/reports/patient/loading.tsx
@@ -0,0 +1,9 @@
+// "use client";
+// import PatientTableSkeleton from "@/components/PatientTableSkeleton";
+// import React from "react";
+
+// const loading = () => {
+//   return <PatientTableSkeleton />;
+// };
+
+// export default loading;
