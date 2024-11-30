96ce3b267f32e2e219715ac128d5d2bc57370e85
nischal1101 <kaflenk123@gmail.com>
2024-11-30 09:14:36 +0545

    add fetch hospital patients function

---
+++ src/utils/FetchHospitalPatients.ts
@@ -0,0 +1,28 @@
+import db from "@/lib/db/db";
+import {
+  hospitalPatients,
+  PatientTable,
+  UserTable
+} from "@/lib/db/Schema";
+import { desc, eq } from "drizzle-orm";
+
+export async function getHospitalPatients(hospitalId: number) {
+  const patients = await db
+    .select({
+      patientId: PatientTable.id,
+      patientName: PatientTable.patientName,
+      dob: PatientTable.dob,
+      phone: PatientTable.phone,
+      email: UserTable.email,
+      registrationDate: hospitalPatients.registrationDate,
+      status: hospitalPatients.status,
+      dischargeDate: hospitalPatients.dischargeDate,
+    })
+    .from(hospitalPatients)
+    .innerJoin(PatientTable, eq(hospitalPatients.patientId, PatientTable.id))
+    .innerJoin(UserTable, eq(PatientTable.userId, UserTable.id))
+    .where(eq(hospitalPatients.hospitalId, hospitalId))
+    .orderBy(desc(hospitalPatients.registrationDate));
+
+  return patients;
+}
